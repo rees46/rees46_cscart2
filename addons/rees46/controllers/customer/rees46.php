30fe0e67bb7bc7280bc75b28a665d0b3b0ea6cfe
Anton Zhavoronkov <anton.zhavoronkov@gmail.com>
2014-12-23 15:55:58 +0300

    Null product fix

--- addons/rees46/controllers/customer/rees46.php
+++ addons/rees46/controllers/customer/rees46.php
@@ -10,18 +10,20 @@ if ($mode == 'get_info') {
     foreach ($ids as $id) {
         $p_info = fn_get_product_data($id, $auth, CART_LANGUAGE, '', false, true, false, false);
 
-        $p = Array(
-            'name' => $p_info['product'],
-            'url' => fn_url('products.view?product_id='.$id),
-            'price' => round($p_info['base_price'], 2),
-            'image_url' => $p_info['main_pair']['icon']['image_path']
-        );
+        if ($p_info['product'] != null) {
+            $p = Array(
+                'name' => $p_info['product'],
+                'url' => fn_url('products.view?product_id='.$id),
+                'price' => round($p_info['base_price'], 2),
+                'image_url' => $p_info['main_pair']['icon']['image_path']
+            );
 
-        if ($p['image_url'] == null) {
-            $p['image_url'] = $p_info['main_pair']['detailed']['image_path'];
+            if ($p['image_url'] == null) {
+                $p['image_url'] = $p_info['main_pair']['detailed']['image_path'];
+            }
+
+            array_push($products, $p);
         }
-
-        array_push($products, $p);
     }
 
     header('Content-Type: application/json');
